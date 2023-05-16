9463acd8ca3721845b25ea56fc78f0f0e4a79dd3
Tambet Viitkar <tambet@viitkar.ee>
2023-05-17 00:22:43 +0300

    Added code to reformat and output input MAC address in all three requested formats.
    Added code that uses the API of macvendor.com to get the vendor name.

--- mac_address.py
+++ mac_address.py
@@ -1,25 +1,60 @@
 import re
+import requests
 from flask import Flask, jsonify
 
 MAC_ADDRESS_BIT_LENGTH = 48  # mac addresses consist of 48 bits
+MAC_FORMATS = {0: None, 2: ":", 4: "."}  # key represents group size, value represents group separator
+
 
 app = Flask(__name__)  # initialize flask
 
 @app.route('/verify-mac-address/<address>', methods=["GET"])
 def verify_mac_address(address):
+    reformatted_macs = []  # list for reformatted mac addresses
+    mac_vendor = ""
     
     # remove group separators (dot, semicolon, dash, etc)
     address_no_grouping = re.sub('[.:]', "", address)  # substitute can be easily updated
 
-    # convert string into a hex format
-    address_base16 = int(address_no_grouping, 16)
+    response = "NOK"  # default assumption is that input value is invalid
+    if len(address_no_grouping) == 12:  # mac addresses are 12 character in length
+
+        # convert string into a hex format
+        address_base16 = int(address_no_grouping, 16)  # todo: catch error!
     
-    response = "OK"
-    if not address_base16.bit_length() <= MAC_ADDRESS_BIT_LENGTH:
-        response = "NOK"
-    
+        # todo: reserved and/or special case mac addresses (e.g. broadcast)
+        if address_base16.bit_length() <= MAC_ADDRESS_BIT_LENGTH:
+            response = "OK"
+        
+            for group_size in MAC_FORMATS.keys():
+                mac_grouping = []  # temporary list to store mac split mac address
+                
+                if MAC_FORMATS[group_size]:
+                    # split mac address every n-th character based on the mac format key-value pair
+                
+                    for idx in range(0, len(address_no_grouping), group_size):
+                        mac_grouping.append(address_no_grouping[idx:idx + group_size])
+                
+                    # join the previously split mac address list into a string
+                    reformatted_macs.append(f"{MAC_FORMATS[group_size]}".join(mac_grouping))
+                else:
+                    # one mac address doesn't need any formatting
+                    reformatted_macs.append(address_no_grouping)
+            
+            # api call to get vendor
+            # todo: idea - create a separate function and instead call it here (?)
+            api_call_response = requests.get(f"https://api.macvendors.com/{address_no_grouping}")
+            if api_call_response.status_code == 200:
+                mac_vendor = api_call_response.text
+            else:
+                mac_vendor = "Not found"
+    else:
+        pass
+        
     # return response in json format
-    return jsonify({"result": response})  
+    return jsonify({"result": response,
+                    "formats": reformatted_macs,
+                    "vendor": mac_vendor})  
 
 if __name__ == "__main__":
     app.run()
